369013094c010a639fe737f132a00de50d2d9add
DestinyofYeet <ole@ole.blue>
2026-08-06 12:02:24 +0200

    chore(version): test

--- nix/pkg.nix
+++ nix/pkg.nix
@@ -17,7 +17,7 @@ rustPlatform.buildRustPackage {
 
   src = ../.;
 
-  cargoHash = "sha256-70WpptTuCEBeAIeTt/JZfDuWz10CvR8U3MuM3Mqo8+E=";
+  cargoHash = "sha256-y0csxyTRRWeD1CYtUlCV5tmV/mgUksPTlNlBIV1vtks=";
 
   meta = with lib; {
     description = toml.package.description;
