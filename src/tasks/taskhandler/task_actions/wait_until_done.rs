63adbf911fd4b23cc7e008a765b67a8b7f637b03
DestinyofYeet <ole@ole.blue>
2026-08-15 01:50:48 +0200

    fix(task_actions): reliably tell if task is done when calling wait_until_done

--- src/tasks/taskhandler/task_actions/wait_until_done.rs
+++ src/tasks/taskhandler/task_actions/wait_until_done.rs
@@ -3,6 +3,7 @@ use std::sync::mpsc::channel;
 use crate::{
     server::{database_strategy::DatabaseStrategy, memory_strategy::MemoryStrategy},
     tasks::{
+        task::TaskState,
         taskhandler::{
             TaskEvent, TaskHandlerError, TaskSubscriberEvent, task_actions::TaskActions,
         },
@@ -18,10 +19,28 @@ where
     pub fn wait_until_done<T>(&self, task: &TaskRef<T, D, ME>) -> Result<(), TaskHandlerError> {
         let (tx, rx) = channel();
 
+        if task.get_state() == TaskState::Done {
+            return Ok(());
+        }
+
         self.to_task_handler.send(TaskEvent::RegisterSubscriber {
             for_task: task.get_id(),
             subscriber: tx,
         })?;
+
+        // There can be the following issue:
+        // - First check if task is done
+        // - Worker sends Done notification
+        // - Register the listener
+        // - :( we don't get the signal
+        //
+        // We check a second time here, just in case the above happened
+        // There should be a proper fix at some point, because if this happens too often,
+        // it could exhaust the memory of the system, since the uuid and channel tx are stored in a hashmap.
+        // But for now it should be a good enough fix to reliably call wait_until_done and be sure we don't get stuck
+        if task.get_state() == TaskState::Done {
+            return Ok(());
+        }
 
         while let Some(message) = rx.iter().next() {
             match message {
