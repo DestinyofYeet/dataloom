9b875ed38dde864ec09724874a84650042812998
DestinyofYeet <ole@ole.blue>
2026-08-18 17:15:04 +0200

    feat(memory_strategy): adds modify option

--- src/server/memory_strategy/default_strategies/local_storage/impl_trait.rs
+++ src/server/memory_strategy/default_strategies/local_storage/impl_trait.rs
@@ -1,16 +1,16 @@
-use serde::de::DeserializeOwned;
+use serde::{Serialize, de::DeserializeOwned};
 
 use crate::server::memory_strategy::{
     MemoryError, MemoryStrategy, default_strategies::local_storage::definition::LocalMemory,
 };
 
 impl MemoryStrategy for LocalMemory {
-    fn store_key<T>(&self, key: &str, item: T) -> Result<(), MemoryError>
+    fn store_key<T>(&'static self, key: &str, item: &T) -> Result<(), MemoryError>
     where
         T: serde::Serialize + std::fmt::Debug,
     {
         let string =
-            serde_json::to_string(&item).map_err(|e| MemoryError::Storage(e.to_string()))?;
+            serde_json::to_string(item).map_err(|e| MemoryError::Storage(e.to_string()))?;
 
         let mut map = self
             .storage
@@ -23,7 +23,7 @@ impl MemoryStrategy for LocalMemory {
     }
 
     fn retrieve_key<T>(
-        &self,
+        &'static self,
         key: &str,
     ) -> Result<Option<T>, crate::server::memory_strategy::MemoryError>
     where
@@ -45,5 +45,51 @@ impl MemoryStrategy for LocalMemory {
         };
 
         Ok(result)
+    }
+
+    fn modify_key_mut<'a, T, F, RES>(
+        &'static self,
+        key: &str,
+        func: F,
+    ) -> Result<Option<RES>, MemoryError>
+    where
+        T: serde::Deserialize<'a> + std::fmt::Debug + Serialize,
+        F: FnOnce(&mut T) -> RES,
+    {
+        let mut map = self
+            .storage
+            .lock()
+            .map_err(|e| MemoryError::Retrieve(format!("Failed to get lock: {e}")))?;
+
+        match map.remove_entry(key) {
+            // SAFETY: In my head this should be fine.
+            //
+            // We leak `value` to the stack so serde is happy with a static lifetime.
+            // We do work in func()
+            // We re-encode the modified value back to json.
+            // We save the json back to the map.
+            // We take the static pointer and cast it back to a box and drop it
+            //
+            // This could technically break if in func(), the user returns the `&mut` pointer out as `RET`, but I think the rust borrowchecker should catch that.
+            Some((key, value)) => unsafe {
+                let mut value: &'static str = Box::leak(Box::new(value));
+
+                let mut item: T = serde_json::from_str(value)
+                    .map_err(|e| MemoryError::Retrieve(e.to_string()))?;
+
+                let result = func(&mut item);
+
+                let json = serde_json::to_string(&item)
+                    .map_err(|e| MemoryError::Storage(e.to_string()))?;
+
+                map.insert(key, json);
+
+                drop(Box::from_raw(&mut value));
+
+                Ok(Some(result))
+            },
+
+            None => Ok(None),
+        }
     }
 }
--- src/server/memory_strategy/definition.rs
+++ src/server/memory_strategy/definition.rs
@@ -1,22 +1,35 @@
-use serde::{Serialize, de::DeserializeOwned};
+use serde::{Deserialize, Serialize, de::DeserializeOwned};
 
 use crate::server::memory_strategy::MemoryError;
 
 pub trait MemoryStrategy: Send + Sync {
-    fn store<T>(&self, item: T) -> Result<(), MemoryError>
+    fn store_key<T>(&'static self, key: &str, item: &T) -> Result<(), MemoryError>
+    where
+        T: Serialize + std::fmt::Debug;
+
+    fn retrieve_key<T>(&'static self, key: &str) -> Result<Option<T>, MemoryError>
+    where
+        T: DeserializeOwned + std::fmt::Debug;
+
+    fn modify_key_mut<'a, T, F, RES>(
+        &'static self,
+        key: &str,
+        func: F,
+    ) -> Result<Option<RES>, MemoryError>
+    where
+        T: Deserialize<'a> + std::fmt::Debug + Serialize,
+        F: FnOnce(&mut T) -> RES;
+
+    fn store<T>(&'static self, item: &T) -> Result<(), MemoryError>
     where
         T: Serialize + std::fmt::Debug,
     {
         let key = std::any::type_name::<T>();
 
-        self.store_key(key, item)
+        self.store_key(key, &item)
     }
 
-    fn store_key<T>(&self, key: &str, item: T) -> Result<(), MemoryError>
-    where
-        T: Serialize + std::fmt::Debug;
-
-    fn retrieve<T>(&self) -> Result<Option<T>, MemoryError>
+    fn retrieve<T>(&'static self) -> Result<Option<T>, MemoryError>
     where
         T: DeserializeOwned + std::fmt::Debug,
     {
@@ -24,8 +37,4 @@ pub trait MemoryStrategy: Send + Sync {
 
         self.retrieve_key(key)
     }
-
-    fn retrieve_key<T>(&self, key: &str) -> Result<Option<T>, MemoryError>
-    where
-        T: DeserializeOwned + std::fmt::Debug;
 }
